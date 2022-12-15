1e565ef592e3961a1126affc524c5653d7edc005
Maryna Dzuh <maryna.dzuh@gmail.com>
2022-12-15 23:50:50 +0200

    creat

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -6,7 +6,7 @@ async function fetchImages(query, page) {
   const KEY = `31735095-684ab1f66144313a79ef81b6d`;
   const url = `https://pixabay.com/api/`;
   
-  const response = await axios.get(`${url}?key=${KEY}&q=${query}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${page}`);
+  const response = await axios.get(`${url}?key=${KEY}&q=${query}&orientation=horizontal&safesearch=true&image_type=photo&per_page=9&page=${page}`);
   if(!response.data.total || !query)
   {throw new Error(`Find ${ response.data.total } matches`)}
   return (response.data);
